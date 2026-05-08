149c7f7bd574d3b8df3e60f46ae7bb43ff80c8c3
VarrioSurenos <mastercrafternathan1@gmail.com>
2026-05-08 03:38:42 -0700

    Reverted

--- src/site/config.js
+++ src/site/config.js
@@ -46,12 +46,6 @@ const CONFIG = {
       "url": "mailto:plutonium@fbi.one",
       "icon": "assets/images/t.jpg"
     },
-    {
-      "text": "Github",
-      "subtitle": "VarrioSurenoLocos",
-      "url": "https://github.com/VarrioSurenoLocos",
-      "icon": "M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
-    },
     // Example of a custom link type that handles "copy on click"
     {
       "text": "Discord Name",
@@ -73,10 +67,8 @@ const CONFIG = {
     "shuffle": false,
 
     // Replace these with your Discord CDN MP3 links
-    // One at a time currently have not implemented the multi slector for the audios.
 "tracks": [
   "https://cdn.discordapp.com/attachments/1501069438454075475/1502160176164245594/Vance_Joy_-_Riptide_Official_Video_2.mp3?ex=69feb32e&is=69fd61ae&hm=1f83df7110d31887bb6e013759c507957b706395ab5be7dae56f8631040395d4&"
-  "https://cdn.discordapp.com/attachments/1501069438454075475/1502253021600415754/Bag_Chasin_feat._Lil_Nate.mp3?ex=69ff09a6&is=69fdb826&hm=6b3314e076abb9d7afa8d1cbc54b18bd4c6480b7d9fcb2b54d541ca197455208&"
 ]
   },
 
@@ -147,4 +139,3 @@ const CONFIG = {
     "copyright": "© 2026 Plutonium Development" // Add your copyright text here
   }
 };
-
