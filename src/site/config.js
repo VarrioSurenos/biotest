1f5ceb4848022e03fdfa4046a125588343f54044
Benjamin Netanyahu <mastercrafternathan1@gmail.com>
2026-05-12 13:45:37 -0700

    Update config.js

--- src/site/config.js
+++ src/site/config.js
@@ -82,21 +82,43 @@ const CONFIG = {
 
     // Replace these with your Discord CDN MP3 links
 "tracks": [
-  "https://file.garden/agE3GkmcYA8xD0gx/Vance%20Joy%20-%20'Riptide'%20Official%20Video%20(2)%20(2).mp3",
-  
-  "https://file.garden/agE3GkmcYA8xD0gx/Bag%20Chasin%20(feat.%20Lil%20Nate)%20(2).mp3",
 
-  "https://file.garden/agE3GkmcYA8xD0gx/The%20Neighbourhood%20-%20Sweater%20Weather%20(Lyrics)%20(1).mp3",
-
-  "https://file.garden/agE3GkmcYA8xD0gx/Bella%20Kay%2C%20Lonely%20(Lyrics).mp3",
-
-  "https://file.garden/agE3GkmcYA8xD0gx/Bella%20Kay%20-%20The%20Sick%20(Lyrics)%20%5BI1j1GJyOJ3g%5D.mp3",
-
-  "https://file.garden/agE3GkmcYA8xD0gx/Dominic%20Fike%20Baby%20Doll%20(Official%20Audio).mp3",
-
-  "https://file.garden/agE3GkmcYA8xD0gx/Oliver%20Anthony%20-%20Rich%20Men%20North%20Of%20Richmond%20(Lyrics).mp3"
-]
+  {
+    "name": "Riptide",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/Vance%20Joy%20-%20'Riptide'%20Official%20Video%20(2)%20(2).mp3"
   },
+
+  {
+    "name": "Bag Chasin",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/Bag%20Chasin%20(feat.%20Lil%20Nate)%20(2).mp3"
+  },
+
+  {
+    "name": "Sweater Weather",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/The%20Neighbourhood%20-%20Sweater%20Weather%20(Lyrics)%20(1).mp3"
+  },
+
+  {
+    "name": "Lonely",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/Bella%20Kay%2C%20Lonely%20(Lyrics).mp3"
+  },
+
+  {
+    "name": "The Sick",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/Bella%20Kay%20-%20The%20Sick%20(Lyrics)%20%5BI1j1GJyOJ3g%5D.mp3"
+  },
+
+  {
+    "name": "Baby Doll",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/Dominic%20Fike%20Baby%20Doll%20(Official%20Audio).mp3"
+  },
+
+  {
+    "name": "Rich Men North Of Richmond",
+    "url": "https://file.garden/agE3GkmcYA8xD0gx/Oliver%20Anthony%20-%20Rich%20Men%20North%20Of%20Richmond%20(Lyrics).mp3"
+  }
+
+]
 
   // ---------------------------------------------------------------------------
   // 3. SCHEDULE & STATUS
